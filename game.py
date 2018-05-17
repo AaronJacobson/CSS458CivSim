f01a08815a78233a4cf15b0dc0b53efc0daf2157
Aaron Jacobson <aaronlj@uw.edu>
2018-05-17 13:59:31 -0700

    filled out some of the buildings in initBuildingLookup

--- game.py
+++ game.py
@@ -20,10 +20,14 @@ class Game(object):
         #TODO List of grids\
         
     def initBuildingLookUp(self):
-        granary = Building(building_name="granary",gold_yield=-1,gold_bonus=0,food_yield=2,\
-        food_bonus=0,science_yield=0,science_bonus=0,science_pop_bonus=0,\
-        prod_yield=0,prod_bonus=0)
-        
+        granary = Building(building_name="granary",gold_yield=-1,food_yield=2)
+        library = Building(building_name="library",gold_yield=-1,science_pop_bonus=.5)
+        stoneworks = Building(building_name="stoneworks",gold_yield=-1,prod_yield=1)
+        watermill = Building(building_name="watermill",gold_yield=-2,food_field=2,prod_yield=1)
+        market = Building(building_name="market",gold_yield=2,gold_bonus=.25)
+        university = Building(building_name="university",gold_yield=-2,science_bonus=.33)
+        workshop = Building(building_name="workshop",gold_yield=-2,prod_yield=2,prod_bonus=.1)
+        bank = Building(building_name="bank",gold_yield=2,gold_bonus=.25)
         self.building_lookup = { granary.building_name:granary }
         
         #TODO List of grids
