de062744968f40a0a1ead40e5e3c51a65e6421ac
Aaron Jacobson <aaronlj@uw.edu>
2018-05-17 13:43:17 -0700

    changed paramter names

--- building.py
+++ building.py
@@ -1,12 +1,13 @@
 class Building(object):
     
-    def __init__(self,gold,gold_bonus,food,food_bonus,science, \
-    science_bonus,production,production_bonus):
-        self.gold = gold
+    def __init__(self,gold_yield=0,gold_bonus=0,food_yield=0,food_bonus=0,science_yield=0, \
+    science_bonus=0,science_pop_bonus=0,production_yield=0,production_bonus=0):
+        self.gold = gold_yield
         self.gold_bonus = gold_bonus
-        self.food = food
+        self.food = food_yield
         self.food_bonus = food_bonus
-        self.science = science
+        self.science = science_yield
         self.science_bonus = science_bonus
-        self.production = production
+        self.science_pop_bonus = science_pop_bonus
+        self.production = production_yield
         self.production_bonus = production_bonus
