58363d979a3509e9ff30e3a9e564aeb13004f2d1
Aaron Jacobson <alj@nym.hush.com>
2018-05-22 18:57:53 -0700

    Added Tile.get_neighbors and added self to the improve_blank functions

--- tile.py
+++ tile.py
@@ -24,13 +24,13 @@ class Tile(object):
     on following turn, processing turn will increase number to 1 to represent
     a built road. Railroad will be represented the same way, but with 1.5 and 2.
     """
-    def build_road():
+    def build_road(self):
         self.road = 0.5
-    def build_railroad():
+    def build_railroad(self):
         if(self.road == 1):
             self.road = 1.5
             
-    def process_turn():
+    def process_turn(self):
         if(self.road == 0.5):
             self.road = 1
         if(self.road == 1.5):
@@ -38,15 +38,37 @@ class Tile(object):
         #TODO add processing new improvement for the turn
         
     #Class calls with amount needed to improve resource
-    def improve_food(amount):
+    def improve_food(self,amount):
         self.food_yield += amount
-    def improve_prod(amount):
+    def improve_prod(self,amount):
         self.prod_yield += amount
-    def improve_science(amount):
+    def improve_science(self,amount):
         self.science_yield += amount
-    def improve_gold(amount):
+    def improve_gold(self,amount):
         self.gold_yield += amount
         
         #TODO init yields based on elevation and biome
         #TODO manage roads, won't change yields
         #TODO manage improvements
+
+    def get_neighbors(self):
+        list_of_neighbors = []
+        if self.y > 0:
+            list_of_neighbors.append(self.grid.tiles[self.y-1,self.x])
+            if self.y % 2 == 0:
+                #even row
+                list_of_neighbors.append(self.grid.tiles[self.y-1,self.x-1])
+            else:
+                #odd row
+                list_of_neighbors.append(self.grid.tiles[self.y-1,self.x+1])#TODO check if it's on the edge of the map
+        list_of_neighbors.append(self.grid.tiles[self.y,self.x-1])
+        list_of_neighbors.append(self.grid.tiles[self.y,self.x+1])#TODO check if it's on the edge of the map
+        if self.y < self.grid.y - 1:
+            list_of_neighbors.append(self.grid.tiles[self.y+1,self.x])
+            if self.y % 2 == 0:
+                #even row
+                list_of_neighbors.append(self.grid.tiles[self.y+1,self.x-1])
+            else:
+                #odd row
+                list_of_neighbors.append(self.grid.tiles[self.y+1,self.x+1])#TODO check if it's on the edge of the map
+        return list_of_neighbors
