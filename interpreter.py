2f275079b6c2b3fa00c839003018c125ad6df035
MichaelVaschillo <mishav@uw.edu>
2018-05-22 20:40:54 -0700

    adding to interperater
    should store cities info in file
    working on grid info next

--- interpreter.py
+++ interpreter.py
@@ -1,18 +1,51 @@
+
 class Interpreter(object):
 
     def __init__(self):
         return
 
-    def write(self, grid):
+    def write(self, game):
+        #self.gridFile = game.grid
+        self.mapFile = open("testMap","w")
+        self.civFile = open("testCiv","w")
+        self.cityFile = open("testCities","w")
+        self.unitFile = open("testUnits", "w")
+
+        #get from grid and write all the info about each tile
+        for self.x in range(game.grid.x):
+            for self.y in range(game.grid.y):
+                mapFile.write(self.x + " " + self.y + " ") #add grid features like terrain and stuff
+
+        #get from Civs and write all info about each civ
+        for self.civNum in game.civs:
+            civsFile.write(self.civNum + " ")
+
+        #get from Cities and write all info about each cities
+        for self.city in grid.cities:
+            citiesFile.write(self.city.x + " " + self.city.y + " " + self.city.civ + ""
+            + self.city.pop + " " + self.city.food + " " + self.city.production + " "
+            + self.city.health + " " + self.city.strength + " ")
+            for self.building in self.city.building_list:
+                citiesFile.write(self.building + "$")
+            citiesFile.write("EOL")
+        #--------------------------------------------------------
+
+        #get from Units and write all info about each units
+        for self.unit in grid.units:
+            unitFile.write(self.unit.name + " " + )
+
+
+
         return
 
-    def read(self, gameNumber, turnNumber, game):
+    def read(self, game):
+
         #varify that variables is indeed what they should be
         #if(game)
 
         #varify that gameNum is resonable
-        if(type(gameNumber) is int):
-            if(gameNumber > 0):
+        if(type(game.gameNumber) is int):
+            if(game.gameNumber > 0):
                 gameNum = gameNumber
             else:
                 #Exception
