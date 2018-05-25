1417b5d9b373967d55f356f58a426fbb10136b3f
Connor Browne <Pankzers@gmail.com>
2018-05-24 19:16:48 -0700

    minor editing

--- building.py
+++ building.py
@@ -1,5 +1,6 @@
 class Building(object):
     
+    
     def __init__(self,name="none",gold_yield=0,gold_bonus=0.0,food_yield=0,food_bonus=0.0,
     science_yield=0,science_bonus=0.0,science_pop_bonus=0,prod_yield=0,prod_bonus=0.0):
         self.name = name
@@ -12,3 +13,6 @@ class Building(object):
         self.science_pop_bonus = science_pop_bonus
         self.production = prod_yield
         self.production_bonus = prod_bonus
+    
+        
+    
