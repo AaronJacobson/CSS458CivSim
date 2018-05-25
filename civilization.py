4d9084b9d6fd276df2f61b336760a833170a4534
Connor Browne <Pankzers@gmail.com>
2018-05-24 18:03:05 -0700

    Added civNum as a parameter to Civ to make dataplotting easier.

--- civilization.py
+++ civilization.py
@@ -1,6 +1,7 @@
 class Civ(object):
     
-    def __init__(self):
+    def __init__(self,civNum):
+        self.civNum = civNum
         self.unit_list = []
         self.city_list = []
         self.science = 0
