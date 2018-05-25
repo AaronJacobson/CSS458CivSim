efded16cd577e222ce2fa4ff0d7fbd77bf32fc20
Connor Browne <Pankzers@gmail.com>
2018-05-24 20:49:43 -0700

    Fleshed out run method and updated simInit method

--- game.py
+++ game.py
@@ -22,7 +22,7 @@ class Game(object):
         #Initialize tile grids and civ list
         self.x = x
         self.y = y
-        self.civs = None
+        self.civs = []
         self.turns = []
         self.curGrid = None
         
@@ -33,7 +33,7 @@ class Game(object):
         #TODO List of grids\
     
     
-    def simInit(self,mapName="DefaultMap"):
+    def simInit(self):
         """
         """
         #RUN MAP GENERATOR HERE TO POPULATE INITIAL GRID!
@@ -44,17 +44,22 @@ class Game(object):
     def run(self):
         """
         """
+        yield_vals = N.zeros((self.numTurns,len(self.civs),4))
         #Initialize run loop
         for i in range(self.numTurns):
             #Grab Grid for Processing
             curGrid = self.turns[-1]
-
-            #Process Civs
-            #Stuff goes here!
-
+            
+            #Process Civ Wars
+            
+            #Process Civs Individual turns
+            for civ in self.civs:
+                yield_vals[i,civ.civNum]=civ.process_turn()
+            
             #Process Tiles
             #Gods this is inefficient, but without creating a list of tile changes, cellular automata is the way to go!
             for m in range(y):
                 for n in range(x):
                     curGrid[m][n].process_turn()
             #Update State Variables if any exist
+            self.turns.append(curGrid)
