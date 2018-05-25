1b5c4cada207f344a753086db61e45a43db96774
Connor Browne <Pankzers@gmail.com>
2018-05-24 19:42:11 -0700

    Added a strength_mod attribute to improvement so that forts can exist

--- improvement.py
+++ improvement.py
@@ -3,9 +3,10 @@ class Improvement(object):
     """
     """
     
-    def __init__(self,name="none",gold_yield=0,food_yield=0,science_yield=0,prod_yield=0,):
+    def __init__(self,name="none",gold_yield=0,food_yield=0,science_yield=0,prod_yield=0,strength_mod=0.0):
         self.name = name
         self.gold_yield = gold_yield
         self.food_yield = food_yield
         self.science_yield = science_yield
         self.prod_yield = prod_yield
+        self.strength_mod = strength_mod
