876a94a67461b10efd499e6bae1da103d0de4ea1
Connor Browne <Pankzers@gmail.com>
2018-05-27 13:20:27 -0700

    Implemented initCivs method

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ from grid import Grid
 from civilization import Civ
 from building import Building
 from unit import Unit
+from city import City
+import classlookup
 #from interpreter import Interpreter
 import numpy as N
 import os
@@ -13,7 +15,7 @@ class Game(object):
     """
     """
 
-    def __init__(self,y = 50,x = 100,num_turns = 500,map_name = None, num_civ = 0):
+    def __init__(self,y = 50,x = 100,num_turns = 500, num_civ = 0):
         """
         """
 
@@ -27,12 +29,34 @@ class Game(object):
         self.turns = []
 
         self.cur_grid = Grid(y,x)
+        self.initCivs(num_civ)
 
         #TODO initialize the list/dictionary of biomes and they're yields
         #TODO List of grids\
 
-    def initCivs(num_civ):
-        pass
+    def initCivs(self,num_civ):
+        """
+        """
+        if num_civ // 2 <= 3:
+            xdiv = 4
+            ydiv = 3
+        else:
+            xdiv=4
+            ydiv=4
+        for i in range(num_civ):
+            self.civs.append(Civ(i))
+            xrand = N.random.normal(0,0.05*self.x)
+            yrand = N.random.normal(0,0.05*self.y)
+            print(xrand)
+            print(yrand)
+            #self.civs[i].city_list.append(City(self.cur_grid,((self.x//xdiv)*((i+1)%4))+xrand,(self.y//ydiv*((i//ydiv)+1))+yrand,self.civs[i]))
+            print(int((self.y//ydiv*((i//ydiv)+1))+yrand))
+            print(int(((self.x//xdiv)*((i+1)%4))+xrand))
+            self.civs[i].city_list.append(City(self.cur_grid,int(self.y//ydiv*((i//ydiv)+1)+yrand),int(((self.x//xdiv)*((i+1)%4))+xrand),self.civs[i]))
+            warrior = classlookup.ClassLookUp.unit_lookup['warrior']
+            warrior_add = Unit(name = warrior.name,atype = warrior.atype,prod_cost = warrior.prod_cost,speed = warrior.speed,y=-1,x=-1,civ=self.civs[i])
+            self.civs[i].mil_unit_list.append(warrior_add)
+                
     
     def run(self):
         """
