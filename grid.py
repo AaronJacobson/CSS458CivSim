f1438338035223735db3c92aa97ee60684a63baf
Connor Browne <Pankzers@gmail.com>
2018-05-27 09:42:53 -0700

    Minor bugfixing for map generation

--- grid.py
+++ grid.py
@@ -4,12 +4,12 @@ from classlookup import ClassLookUp
 
 class Grid(object):
 
-    def __init__(self,y,x,precentGrass=.5,articWidth=3,tundraWidth=5,probForrest=0.1,probJungle=0.1,probRiver = 0.2,probHill = 0.1):
+    def __init__(self,y,x,precentGrass=.5,arcticWidth=0.1,tundraWidth=0.15,probForrest=0.1,probJungle=0.1,probRiver = 0.2,probHill = 0.1):
         self.y = y
         self.x = x
         self.precentGrass = precentGrass
-        self.articWidth = articWidth
-        self.tundraWidth = tundraWidth
+        self.arcticWidth = int(y*arcticWidth)
+        self.tundraWidth = int(y*tundraWidth)
         self.probForrest = probForrest
         self.probJungle = probJungle
         self.probRiver = probRiver
@@ -18,27 +18,27 @@ class Grid(object):
         self.tiles = N.zeros((y,x),dtype='object')
 
         #---------------setting terrain for map---------------------------------
-        #Set both artic circle
-        for row in range(self.articWidth):
+        #Set both arctic circle
+        for row in range(self.arcticWidth):
             for col in range(x):
                 #print("pretest")
-                #top artic circle
-                self.tiles[row,col] = Tile(self,row,col,"artic","none")
-                #bottom artic circle
-                self.tiles[self.y-(1+row),col] = Tile(self,row,col,"artic","none")
+                #top arctic circle
+                self.tiles[row,col] = Tile(self,row,col,"arctic","none")
+                #bottom arctic circle
+                self.tiles[self.y-(1+row),col] = Tile(self,row,col,"arctic","none")
                 #print("test")
 
         #Set both tundric circle
         for row in range(self.tundraWidth):
             for col in range(x):
                 #top tundra circle
-                self.tiles[row,col] = Tile(self,row,col,"tundra","none")
+                self.tiles[row+self.arcticWidth,col] = Tile(self,row,col,"tundra","none")
                 #bottom tundra circle
-                self.tiles[self.y-row-self.articWidth,col] = Tile(self,row,col,"tundra","none")
+                self.tiles[self.y-(1+row+self.arcticWidth),col] = Tile(self,row,col,"tundra","none")
 
         #Set the middle terrain
 
-        for row in range((self.tundraWidth+self.articWidth),(self.y-self.tundraWidth-self.articWidth)):
+        for row in range((self.tundraWidth+self.arcticWidth),(self.y-self.tundraWidth-self.arcticWidth)):
             for col in range(x):
                 #Random initialization of tiles terrain
                 isGrassTile = N.random.binomial(1,self.precentGrass)
@@ -63,8 +63,8 @@ class Grid(object):
                         self.tiles[row,col+1].near_river = True
                     #set terrain
                     if(isForrestTile):
-                        self.tiles[row,col].terrain = "forrest"
-                        self.tiles[row,col+1].terrain = "forrest"
+                        self.tiles[row,col].terrain = "forest"
+                        self.tiles[row,col+1].terrain = "forest"
                     elif(isJungleTile):
                         self.tiles[row,col].terrain = "jungle"
                         self.tiles[row,col+1].terrain = "jungle"
