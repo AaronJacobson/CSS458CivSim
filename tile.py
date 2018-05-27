b6600e3c395ac48d51debb3b60d9b86025923120
hvchou <39331488+hvchou@users.noreply.github.com>
2018-05-26 22:09:02 -0700

    Update tile.py

--- tile.py
+++ tile.py
@@ -169,8 +169,8 @@ class Tile(object):
         return list_of_neighbors
 
     def total_yield(self,food_coefficient=1,prod_coefficient=1,science_coefficient=1,gold_coefficent=1):
-        return self.food_yield * food_coefficient + self.get_prod_yield() * prod_coefficient \
-        + self.get_science_yield() * science_coefficient + self.gold_yield * gold_coefficent
+        return self.get_food_yield() * food_coefficient + self.get_prod_yield() * prod_coefficient \
+        + self.get_science_yield() * science_coefficient + self.get_gold_yield() * gold_coefficent
 
 if __name__ == "__main__":
     #Moved this in here to prevent circular imports
